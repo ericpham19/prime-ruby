b1629b70d68491cca0d65a3c5fe6c439fbd15928
Duy Pham <ericpham19@outlook.com>
2022-09-14 17:10:22 +1000

    finished

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+
+def prime?(number)
+    
+    if number > 1 
+      numbers = (2..number-1).to_a 
+     numbers.none? do |num_to_test|
+      number % num_to_test == 0
+    end
+ else false 
+    end 
+ end 
